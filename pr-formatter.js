bfdf773891edcb248e51297a9510a8c8672e1faa
Chris Longe <chris.longe@ergatta.com>
2023-11-21 12:29:13 -0500

    Refactor func to return a string. Add new case to check for.
    Add function to check for numeric value.

--- pr-formatter.js
+++ pr-formatter.js
@@ -1,26 +1,43 @@
 var titleInput = document.getElementById('pull_request_title');
 var bodyInput = document.getElementById('pull_request_body');
 
-// Format the title and set the value of the title input field
+// Check if a string is numeric
+function isNumeric(str) {
+    return /^\d+$/.test(str);
+}
+
+// Extract the ticket prefix and number from the title and return formatted title, ticketPrefix, and ticketNumber
+// Extract the ticket prefix and number from the title and return formatted title string.
+// It can handle the following formats:
+// - team-name/PREFIX-1234-branch-name-here
+// - my-name/branch-name-here
 function extractTicketAndFormatTitle(title) {
-    let ticketParts = title.split('/')[1].split(' ').slice(0, 2);
-    let ticketPrefix = ticketParts[0].toUpperCase();
-    let ticketNumber = ticketParts[1];
-    let description = title.split('/')[1].split(' ').slice(2).join(' ');
+    let secondPart = title.split('/')[1];
+    let ticketParts = secondPart.slice(0, 2);
+    let ticketPrefix = '';
+    let ticketNumber = '';
+
+    if (isNumeric(ticketParts[1])) {
+        console.log('is numeric');
+        ticketPrefix = ticketParts[0].toUpperCase();
+        ticketNumber = ticketParts[1];
+    }
+
+    let description = secondPart.split(' ').slice(2).join(' ');
     description = description.replace(/\b\w/g, l => l.toUpperCase()); // Capitalize each word
-    const formattedTitle = `[Commit Prefix] ${ticketPrefix}-${ticketNumber}: ${description}`;
 
-    // Set the value of the title input field
-    titleInput.value = formattedTitle;
+    const formattedTitle = ticketPrefix && ticketNumber
+        ? `[Commit Prefix] ${ticketPrefix}-${ticketNumber}: ${description}`
+        : `[Commit Prefix] ${description}`;
 
-    // Return ticketPrefix and ticketNumber for use in formatBody
-    return { ticketPrefix, ticketNumber };
+    // Return formattedTitle, ticketPrefix and ticketNumber for use in formatBody
+    return formattedTitle;
 }
 
 // Format the title and body
 function formatTitleAndBody() {
     // Call extractTicketAndFormatTitle and get ticketPrefix and ticketNumber
-    const { ticketPrefix, ticketNumber } = extractTicketAndFormatTitle(titleInput.value);
+    const { formattedTitle, ticketPrefix, ticketNumber } = extractTicketAndFormatTitle(titleInput.value);
 
     // Build the Jira URL
     const jiraUrl = `https://ergatta.atlassian.net/browse/${ticketPrefix}-${ticketNumber}`;
