ce07d3477df3a4c48e4b63e030222b71798fa231
sncraig01 <sarah@ergatta.com>
2024-04-01 15:24:39 -0500

    [bookmarket] make bml work when one commit

--- pr-formatter.js
+++ pr-formatter.js
@@ -1,21 +1,34 @@
 var titleInput = document.getElementById('pull_request_title');
 var bodyInput = document.getElementById('pull_request_body');
+var branchSelectors = document.getElementsByClassName("branch");
 
 // Check if a string is numeric
 function isNumeric(str) {
     return /^\d+$/.test(str);
 }
 
-// Extract the ticket prefix and number from the title and return formatted title string.
-// If a PR has only 1 commit it GitHub uses the commit title as the PR title and the commit body as the PR body. This doesn't cover this case yet.
-// If a PR has more then 1 commit, GitHub uses the branch name as the PR title and leaves the PR body blank.
+// Extract the ticket prefix and number from the branch name and return formatted title string.
+// The branch name is extracted from the dropdown branch selector. If not found, the default PR title is used because
+// if a PR has more then 1 commit, GitHub uses the branch name as the PR title and leaves the PR body blank.
 // - GitHub takes the following branch name: `chris/XX-1234-branch-name-here` and makes the title 'Chris/xx 1234 branch name here'
 // The following branch naming formats are supported:
 // - team-name/PREFIX-1234-branch-name-here
 // - my-name/branch-name-here
 function extractTicketAndFormatTitle(title) {
-    let secondPart = title.split('/')[1];
-    let ticketParts = secondPart.split(' ').slice(0, 2);
+    let branchNameNoFleet = ""
+    let branchParts = []
+
+    if (branchSelectors[1].innerText.includes("compare:")) {
+        let branchName = branchSelectors[1].innerText.replace("compare: ", "")
+        branchNameNoFleet = branchName.split('/')[1];
+        branchParts = branchNameNoFleet.split('-')
+    } else if (title.includes("/")) {
+        let branchName = title
+        branchNameNoFleet = branchName.split('/')[1];
+        branchParts = branchNameNoFleet.split(' ')
+    }
+
+    let ticketParts = branchParts.slice(0,2);
     let ticketPrefix = '';
     let ticketNumber = '';
     let description = '';
@@ -23,9 +36,9 @@ function extractTicketAndFormatTitle(title) {
     if (isNumeric(ticketParts[1])) {
         ticketPrefix = ticketParts[0].toUpperCase();
         ticketNumber = ticketParts[1];
-        description = secondPart.split(' ').slice(2).join(' ');
+        description = branchParts.slice(2).join(' ');
     } else {
-        description = secondPart;
+        description = branchNameNoFleet;
     }
 
     description = description.replace(/\b\w/g, l => l.toUpperCase()); // Capitalize each word
@@ -36,6 +49,7 @@ function extractTicketAndFormatTitle(title) {
 
     return { formattedTitle, ticketPrefix, ticketNumber };
 }
+
 
 // Format the title and body
 function formatTitleAndBody() {
@@ -52,7 +66,6 @@ function formatTitleAndBody() {
     } else {
         formattedBody = `# Description\n\n${bodyInput.value}`;
     }
-   
     // Set the value of the body input field
     bodyInput.value = formattedBody;
 }
